89d6a5a8a999a6f9af1308bd7eab5b5b897b9044
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2021-10-28 18:08:00 +0200

    weapons subtitle update

--- src/Equip.js
+++ src/Equip.js
@@ -24,7 +24,7 @@ class Equip extends React.Component {
         const standard = (
             <>
                 <h3>Your equipment.</h3>
-                <p>and weapons</p>
+                <h4>and weapons</h4>
                 {data.class_char[profession].equip.map((item, index) => {
                     if(typeof item === "string") {
                         return (
